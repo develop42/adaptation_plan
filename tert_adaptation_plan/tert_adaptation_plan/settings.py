ccd598c0e21cb1fde0943da0f5aa7433467e4491
Наталья <Anton>
2018-09-24 18:07:26 +0300

    add settings

--- tert_adaptation_plan/tert_adaptation_plan/settings.py
+++ tert_adaptation_plan/tert_adaptation_plan/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from django.core.exceptions import ObjectDoesNotExist
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -144,4 +145,7 @@ REST_FRAMEWORK = {
     ]
 }
 
-# from . import settings_instance
+try:
+    from .settings_instance import *
+except ObjectDoesNotExist:
+    pass
