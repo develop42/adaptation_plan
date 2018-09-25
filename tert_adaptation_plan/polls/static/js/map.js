d127f26eb30c8eb8fa838a4ec407aa4320604df6
Наталья <Anton>
2018-09-25 17:05:55 +0300

    add header to map

--- tert_adaptation_plan/polls/static/js/map.js
+++ tert_adaptation_plan/polls/static/js/map.js
@@ -13,4 +13,23 @@ define(['map'], function(map) {
         zoom: 12,
     });
     map.setView(view);
+
+    var zoomToExtentControl = new ol.control.ZoomToExtent({
+		extent: [4178426.79147939987, 7498764.237877314, 4198426.7147939987, 7518764.236877314 ],
+      });
+
+    var osm_default = new ol.layer.Tile({
+        source: new ol.source.OSM()
+      });
+
+    map.addControl(zoomToExtentControl);
+      var controls = map.getControls();
+      var attributionControl;
+      controls.forEach(function (el) {
+        console.log(el instanceof ol.control.Attribution);
+        if (el instanceof ol.control.Attribution) {
+          attributionControl = el;
+        }
+      });
+      map.removeControl(attributionControl);
 })
